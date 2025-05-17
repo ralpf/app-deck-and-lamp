730946a2a6c4c6eeede4fd1a1c46b8c24892d40f
Vladimir Munteanu <ralpff@gmail.com>
2025-05-17 21:24:07 +0300

    refactored frontend, tvcon can switch modes

--- data/script.js
+++ data/script.js
@@ -6,17 +6,18 @@ const Main = {
 
     LoadPaletteNames() {
         // fetch from esp32 the palette names
-        fetch('/tvcon/palette_names')
-            .then(response => response.json())
+        fetch('/tvcon/palette/names')
+            .then(response => response.ok ? response.json() : Promise.reject())
+            .catch(() => ["Red Lava", "Marine Blue", "Test1", "Test2"])  // simulate list
             .then(names => {
                 const container = document.getElementById('palette-list');
                 container.replaceChildren();
                 // create buttons for each item
-                names.forEach(name => {
+                names.forEach((name, idx) => {
                     const btn = document.createElement('button');
                     btn.className = 'color-button';
                     btn.textContent = name;
-                    btn.onclick = () => TVConsole.OnNamedPaletteButton(name);
+                    btn.onclick = () => TVConsole.OnFixedPaletteButton(idx);
                     container.appendChild(btn);
                 });
             })
@@ -25,17 +26,21 @@ const Main = {
 
 
     ModeChanged(mode) {
+        // INIT - trigger pallete name fetch
+        if (paletteNamesLoaded === false && mode === "mode_console") {
+            paletteNamesLoaded = true;
+            this.LoadPaletteNames();
+        }
         // hide all mode control sections
         document.querySelectorAll('.mode-controls').forEach(x => { x.style.display = 'none'; });
         // show the selected mode's controls
         const selectedControls = document.getElementById(`${mode}-controls`);
         if (selectedControls) selectedControls.style.display = 'block';
-        // trigger pallete name fetch
-        if (paletteNamesLoaded === false && mode === "mode_console") {
-            paletteNamesLoaded = true;
-            Main.LoadPaletteNames();
-        }
-    }
+        // first time settings apply
+        if (mode === 'mode_console') TVConsole.TryApplyAllOnce();
+        console.log(`Main Dropdown mode changed to ${mode}`)
+    },
+
 
 }; // ============================================================================== END Main
 
@@ -44,32 +49,20 @@ const Main = {
 const Blazar = {
 
     OnColorButton(button) {
-        // Get the background color from the CSS variable
         const hexColor = getComputedStyle(button).getPropertyValue('--color').trim();
-        // Send the request to the backend
-        fetch(`/mood?color=${encodeURIComponent(hexColor)}`).then((response) => {
-            if (response.ok) console.log(`Color set to ${hexColor}`);
-            else console.error(`Failed to set color: ${hexColor} ${response.status}`);
-        }).catch((error) => console.error('Error:', error));
+        do_fetch(`/mood?color=${encodeURIComponent(hexColor)}`);
     },
 
 
     FlickerToggle(toggle) {
         const flickerFields = document.getElementById("flicker-fields");
         flickerFields.style.display = toggle.checked ? "block" : "none";
-        fetch(`/mood?is_flicker=${toggle.checked ? 1 : 0}`).then((response) => {
-            if (response.ok) { console.log(`Flicker set to ${toggle.checked ? "enabled" : "disabled"}`); }
-            else { console.error("Failed to update flicker mode"); }
-        }).catch((error) => console.error("Error:", error));
+        do_fetch(`/mood?is_flicker=${toggle.checked ? 1 : 0}`);
     },
 
 
     FlickerParamChange(input, param) {
-        const value = input.value;
-        fetch(`/mood?${param}=${value}`).then((response) => {
-            if (response.ok) console.log(`${param} set to ${value}`);
-            else console.error(`Failed to update ${param}`);
-        }).catch((error) => console.error(`Param ${param} Exception:`, error));
+        do_fetch(`/mood?${param}=${input.value}`);
     }
 
 }; // ============================================================================ END Blazar
@@ -78,6 +71,31 @@ const Blazar = {
 
 const TVConsole = {
 
+    isInited: false,
+    palette_buttons_selected: 0,
+    control_hue: 0,
+    control_sat: 255,
+
+
+    TryApplyAllOnce() {
+        if (this.isInited) return;
+        this.isInited = true;
+        // find elements
+        const body = document.getElementById("mode_console-controls");
+        const dropdown = body.querySelector("#submode_console");
+        const bright   = body.querySelector("#val-channel");
+        const blend    = body.querySelector("#blend-speed");
+        // init local globals
+        let request = `/tvcon?blend=${blend.value}&bright=${bright.value}`;
+        do_fetch(request);
+        // init local variables
+        this.control_hue = body.querySelector("#hue-channel").value;
+        this.control_sat = body.querySelector("#sat-channel").value;
+        // trigger dropdown
+        this.OnSubmodeChanged(dropdown.value);
+    },
+
+
     OnSubmodeChanged(submode) {
         const parent = document.getElementById('mode_console-controls');
         // Hide all divs with id starting with "submode_"
@@ -85,53 +103,57 @@ const TVConsole = {
             if (child.id?.startsWith('submode_'))
                 child.style.display = 'none';
         });
-
         // Show the one that matches the selected submode
         const target = document.getElementById(submode);
-        if (target)
-            target.style.display = 'block';
+        if (target) target.style.display = 'block';
+        console.log(`TVConsole change submode = ${submode}`)
+        // Call submode parameters
+        let request = "";
+        if (submode === "submode_mirror_blazar") request = `/tvcon/mirror`; else
+        if (submode === "submode_palette")       request = `/tvcon/palette?idx=${this.palette_buttons_selected}`; else
+        if (submode === "submode_hsv")           request = `/tvcon/hsv?hue=${this.control_hue}&sat=${this.control_sat}`;
+        do_fetch(request);
     },
 
 
-    OnSliderChange(input, param) {
-        const value = input.value;
-        fetch(`/tvcon?${param}=${value}`).then((response) => {
-            if (response.ok) console.log(`${param} set to ${value}`);
-            else console.error(`Failed to update ${param}`);
-        }).catch((error) => console.error(`Param ${param} Exception:`, error));
-    },
-
-
-    OnFixedPaletteButton(button) {
-        const index = button.dataset.idx;
-        fetch(`/tvcon/palette?idx=${index}`).then((response) => {
-            if (response.ok) console.log(`Palette index set to ${index}`);
-            else console.error(`Failed to set Palette index: ${index} ${response.status}`);
-        }).catch((error) => console.error('Error:', error));
+    OnFixedPaletteButton(idx) {
+        do_fetch(`/tvcon/palette?idx=${idx}`);
     },
 
 
     OnNamedPaletteButton(name) {
-        fetch(`/tvcon/palette?name=${encodeURIComponent(name)}`)
-            .then(res => {
-                if (res.ok) console.log(`Palette set by name to ${name}`);
-                else console.error(`Failed to set palette by name: ${name}`);
-            })
-            .catch(err => console.error('Palette set by name error:', err));
+        do_fetch(`/tvcon/palette?name=${encodeURIComponent(name)}`);
     },
 
 
-    OnParamChange(input, param) {
+    OnParamChange(input, subroute, param) {
         const value = input.value;
-        fetch(`/tvcon/palette?${param}=${value}`).then((response) => {
-            if (response.ok) console.log(`Palette param ${param} set to ${value}`);
-            else console.error(`Failed to update Palette param ${param}`);
-        }).catch((error) => console.error(`Param ${param} Exception:`, error));
+        const route = subroute ? `/tvcon/${subroute}` : `/tvcon`;
+        const request = `${route}?${param}=${value}`;
+        do_fetch(request);
+        // fill local variables
+        if (subroute === 'hsv') {
+            if (param === 'hue') this.control_hue = value;
+            if (param === 'sat') this.control_sat = value;
+        }
     }
 
 }; // ======================================================================== END TVConsole
 
 
+// helpers
+
+function do_fetch(request) {
+    fetch(request).then((response) => {
+        if (response.ok) console.log(`Request OK ${request}`);
+        else console.error(`FAIL Reques ${request}`);
+    }).catch((error) => console.error(`Exception on ${request}`, error));
+}
+
+// run on document laod
+document.addEventListener("DOMContentLoaded", function() {
+    Main.ModeChanged(document.getElementById('main-dropdown').value);
+});
 
 // Set default mode
 //controls.innerHTML = modes["mood"];
