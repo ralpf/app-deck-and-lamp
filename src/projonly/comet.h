782136281f1869cd144a1884c82022867a6f3383
Vladimir Munteanu <ralpff@gmail.com>
2026-09-22 00:37:08 +0300

    implement the comet, wip

--- src/projonly/comet.h
+++ src/projonly/comet.h
@@ -9,8 +9,27 @@ class Comet
     LedsStrip& leds;
 
     public:
-    Comet(LedsStrip& target) : leds(target) { }
+    float position;                     // the led at which the comet head is. Can be fractional (for smoot animation)
+    ui8   backgroundScaleDown8 = 128;   // the dimm factor for the background (128 = 0.5)
+    ui8   speed = 16;                   // the speed of the comet
+    ui8   headLenth = 3;                // the size of the comet head
+    ui8   tailLength = 16;              // tail length, in number of affected leds
+    CHSV  headColor;
+    CHSV  tailStartColor;
+    CHSV  tailEndColor;
+    bool  directionLeftToRight;    // direction of propagation
 
     public:
-    void Update();
+    Comet(LedsStrip& target) : leds(target)
+    {
+        // default colors
+        this->headColor      = rgb_2_hsv_slow(CRGB::Azure);
+        this->tailStartColor = rgb_2_hsv_slow(CRGB::Beige);
+        this->tailEndColor   = rgb_2_hsv_slow(CRGB::Red);
+        this->directionLeftToRight = true;
+    }
+
+    public:
+    void Update();                      // called from main loop to update animation
+    void StartOne();                    // call once for one run
 };
